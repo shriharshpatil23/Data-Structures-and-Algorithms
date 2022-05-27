9d571df821151dd9406b02e94656aeba9fef088b
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-05-27 06:32:16 +0530

    Time: 8 ms (9.02%), Space: 5.9 MB (29.45%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int steps = 0;
+        while(num1 && num2){
+            if(num1 >= num2){
+                num1 -= num2;
+                steps++;
+            }else{
+                num2 -= num1;
+                steps++;
+            }
+        }
+        return steps;
+    }
+};
