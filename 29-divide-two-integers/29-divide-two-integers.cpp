aea84d958cee2a0ad3d84801cf16eeb4752d27b3
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-05-30 09:35:29 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.45%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        //using log approach
+//     int sign= (dividend<0 ^ divisor<0) ? -1 : 1; //same ? 0(false) (XOR)
+        
+//         long ldividend=labs(dividend);
+//         long ldivisor=labs(divisor);
+     
+//         long res = exp(log(ldividend)-log(ldivisor));
+//       //  cout<<ldividend<<"/"<<ldivisor<<"="<<res;
+//         if(res>INT_MAX)
+//             return sign==1 ? INT_MAX:INT_MIN;
+//         return sign*(int)res;
+        
+        //bit manipulation
+        
+        int ans = 0;
+
+        // Handle special cases
+        if (divisor == INT_MIN)
+            if (dividend == divisor) return 1;
+            else return 0;
+        
+        if (dividend == INT_MIN)
+            if (divisor == 1) return INT_MIN;
+            else if (divisor == -1) return INT_MAX;
+            else dividend += abs(divisor), ans++; // INT_MIN + abs(divisor) / abs(divisor) = 1 + INT_MIN / abs(divisor)
+
+        // find the sign of the answer
+        int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+        
+        // making both dividend and divisor positive
+        dividend = abs(dividend), divisor = abs(divisor);
+        
+        // main code
+        while(dividend >= divisor) { 
+            int t=1; // multiplier by default 1 since (dividend >= divisor)
+            int curr_dvs=divisor;
+            int curr_dvd=dividend >> 1;
+            
+            while (curr_dvd >= divisor){
+                curr_dvs = curr_dvs << 1;
+                t = t << 1;
+                curr_dvd = curr_dvd>>1;
+            }
+
+            dividend -= curr_dvs;
+            ans += t;
+        }
+        return sign > 0 ? ans : -ans;
+
+    }
+};
