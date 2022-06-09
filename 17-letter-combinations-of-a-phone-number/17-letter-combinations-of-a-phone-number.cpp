2ab6ae902eef7d4c6ecad8bcadf46317c6d667be
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-06-09 18:25:04 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (35.07%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    string m[10]={"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};    
-
-    void helper(string digits,string op,int i,vector<string> &ans){
-        if(i>=digits.size()){
-            ans.push_back(op);
+    vector<string> keys {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    
+    void helper(int i, string tmp, vector<int> dig, vector<string> &ans){
+        if(i==dig.size()){
+            ans.push_back(tmp);
             return;
         }
-        int n = digits[i]-'0';
-        string val = m[n-1];
-        
-        for(int j=0;j<val.size();j++){
-            op.push_back(val[j]);
-            helper(digits,op,i+1,ans);
-            //backtracking Step
-            op.pop_back();
+        string val = keys[dig[i]];
+        for(int k = 0; k<val.size();k++){
+            tmp.push_back(val[k]);
+            helper(i+1,tmp,dig,ans);
+            tmp.pop_back();     //Backtraking step
         }
     }
     
     vector<string> letterCombinations(string digits) {
+        vector<int> dig;
+        if(digits.size()==0)    return {};
+        for(auto x : digits){
+            int tmp = x - '0';
+            dig.push_back(tmp);
+        }
         vector<string> ans;
-        
-        if(digits.size()==0)    return ans;
-        
-        string op ="";
-        int i = 0;
-       // vector<string> m = {"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        
-        helper(digits,op,i,ans);
+        helper(0,"",dig,ans);
         return ans;
     }
 };
