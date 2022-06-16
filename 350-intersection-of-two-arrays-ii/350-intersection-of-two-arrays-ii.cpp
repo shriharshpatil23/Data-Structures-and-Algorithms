2538b0e01cc4b38c54e447009e73024754ba52b9
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-06-16 19:46:49 +0530

    Time: 8 ms (57.61%), Space: 10.1 MB (83.18%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -2,23 +2,41 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
           //using unorderd_map to store  and frequency of one vector 
-        unordered_map<int,int>m;
-        vector<int>ans;
-        for(auto val:nums1)
-        {
-            m[val]++;
-        }
-        //if val from 2nd vector is present in map then
-        //put it in output vector
-        //also decrease the count
-        for(auto val:nums2)
-        {
-            if(m[val]>0)
-            {
-                ans.push_back(val);
-                m[val]--;
+        // unordered_map<int,int>m;
+        // vector<int>ans;
+        // for(auto val:nums1)
+        // {
+        //     m[val]++;
+        // }
+        // //if val from 2nd vector is present in map then
+        // //put it in output vector
+        // //also decrease the count
+        // for(auto val:nums2)
+        // {
+        //     if(m[val]>0)
+        //     {
+        //         ans.push_back(val);
+        //         m[val]--;
+        //     }
+        // }
+        // return ans;
+         sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        int i = 0, j = 0;
+        vector<int> res;
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] == nums2[j]){
+                res.push_back(nums1[i]);
+                i++;
+				j++;
+            }
+            else if(nums1[i] < nums2[j]){
+                i++;
+            }
+            else if(nums1[i] > nums2[j]){
+                j++;
             }
         }
-        return ans;
+        return res;
     }
 };
