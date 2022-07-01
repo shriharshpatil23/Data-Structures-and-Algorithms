e7ef0a380d4ca2d6a01f70056011a28d30789b7e
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-07-01 19:08:23 +0530

    Time: 32 ms (54.19%), Space: 18.3 MB (23.42%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int> left (n,1);
+        vector<int> right (n,1);
+        //for left hand side
+        for(int i=1;i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                left[i]=left[i-1]+1;
+            }
+        }
+        //for right hand side
+        for(int i = n-2;i>=0;i--){
+            if(ratings[i]>ratings[i+1]){
+                right[i] = right[i+1] + 1;
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans += max(left[i],right[i]);
+        }
+        return ans;
+    }
+};
