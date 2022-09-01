ca295c3c49b674ba7f20c28ab68c95b24b26365c
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-09-01 10:49:09 +0530

    Time: 222 ms (39.70%), Space: 86.5 MB (9.98%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,21 +11,31 @@
  */
 class Solution {
 public:
-    
-    int helper(TreeNode * root , int mx){
-        if(root==NULL)  return 0;
+    int ans = 0;
+//     int helper(TreeNode * root , int mx){
+//         if(root==NULL)  return 0;
         
-        int cnt = 0;
-        if(root->val >= mx) cnt++;
-        int curr_max = max(root->val,mx);
-        cnt += helper(root->left,curr_max);
-        cnt += helper(root->right,curr_max);
+//         int cnt = 0;
+//         if(root->val >= mx) cnt++;
+//         int curr_max = max(root->val,mx);
+//         cnt += helper(root->left,curr_max);
+//         cnt += helper(root->right,curr_max);
         
-        return  cnt;
+//         return  cnt;
     
+//     }
+    
+    void preOrder(TreeNode * root, int max){
+        if(root==NULL)  return;
+        
+        if(root->val >= max)    ans++ , max = root->val;
+        preOrder(root->left,max);
+        preOrder(root->right,max);
     }
     
     int goodNodes(TreeNode* root) {
-        return helper(root,INT_MIN);    
+        // return helper(root,INT_MIN); 
+        preOrder(root,INT_MIN);
+        return ans;
     }
 };
