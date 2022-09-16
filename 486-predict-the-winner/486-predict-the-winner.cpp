d80acd05c24ff4ffe59fae4c2d05c6d578db5ea2
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-09-16 17:51:54 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (96.93%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,20 +1,24 @@
 #define ll long long
 class Solution {
 public:
+    ll t[2][21][21];
     ll helper(int start, int end, vector<int>&nums,int player){
         if(start>end)  return 0;
+        
+        if(t[player][start][end]!=-1)   return t[player][start][end];
         
         if(player){
             ll play_1_left = nums[start] + helper(start+1,end,nums,0);
             ll play_1_right = nums[end] + helper(start,end-1,nums,0);
-            return max(play_1_left,play_1_right);
+            return t[player][start][end] = max(play_1_left,play_1_right);
         }else{
             ll play_2_left = -nums[start] + helper(start+1,end,nums,1);
             ll play_2_right = -nums[end] + helper(start,end-1,nums,1);
-            return min(play_2_left,play_2_right);
+            return t[player][start][end]=min(play_2_left,play_2_right);
         }
     }
     bool PredictTheWinner(vector<int>& nums) {
+        memset(t,-1,sizeof(t));
         return helper(0,nums.size()-1,nums,1)>=0;
     }
 };
