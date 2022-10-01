d6b49cc5c68d388952dba5cf3b73f24ec7358ab3
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-10-01 10:51:29 +0530

    Time: 4 ms (47.86%), Space: 7.4 MB (20.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int t[101];
-    int helper(int i , string s){
+    int dp[101];
+    int helper(int i, string s){
         if(i==s.size())return 1;
+        
         if(s[i]=='0')return 0;
+        
         if(i>=s.size()-1)return 1;
-        string b = s.substr(i,2);
         
-        if(t[i]!= -1)   return t[i];
+        if(dp[i]!=-1)   return dp[i];
         
-        int way1 = helper(i+1,s);
-        int way2=0;
-        if(stoi(b)<=26&&stoi(b)>0){
-            way2 = helper(i+2,s);
+        string sub = s.substr(i,2);
+        int ways1 = helper(i+1,s);
+        int ways2 = 0;
+        if(stoi(sub)>0 && stoi(sub)<=26){
+            ways2=helper(i+2,s);
         }
-        return t[i] = way1+way2;
+        return dp[i]=ways1+ways2;
     }
-    
     int numDecodings(string s) {
-        memset(t,-1,sizeof(t));
-       return helper(0,s);        
+        if(s=="0")  return 0;
+        memset(dp,-1,sizeof(dp));
+        return helper(0,s);
     }
 };
