f141794cc1047db4e515cb8c3b5338b1fc2e4c3b
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-10-18 10:34:58 +0530

    Time: 85 ms (74.89%), Space: 28.7 MB (56.89%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    
+    /// Simple DFS ////
 //     int m,n;
 //     bool vis[41][41];
 //     bool isValid(int i, int j, vector<vector<int>>&grid){
@@ -56,52 +58,96 @@ public:
 //         n=grid[0].size();
 //         return dfs(m-1,n-1,grid,k);
     
-    
-     int solve(vector<vector<int>>& grid,int k){
-		// At a particular cell we will store the number of obstacles that we can still remove after walking through that cell
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
-        queue<vector<int>> q;
+    /// Simple BFS Solution /////
+//      int solve(vector<vector<int>>& grid,int k){
+// 		// At a particular cell we will store the number of obstacles that we can still remove after walking through that cell
+//         vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size(),-1));
+//         queue<vector<int>> q;
 		
-		// queue stores (x,y,current path length,number of obstacles we can still remove)
-        q.push({0,0,0,k});
-        while(!q.empty()){
-            auto t=q.front();
-            int x=t[0],y=t[1];
-            q.pop();
+// 		// queue stores (x,y,current path length,number of obstacles we can still remove)
+//         q.push({0,0,0,k});
+//         while(!q.empty()){
+//             auto t=q.front();
+//             int x=t[0],y=t[1];
+//             q.pop();
 			
-			// Exit if current position is outside of the grid
-            if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
-                continue;
-            }
+// 			// Exit if current position is outside of the grid
+//             if(x<0 || y<0 || x>=grid.size() || y>=grid[0].size()){
+//                 continue;
+//             }
 			
-			// Destination found
-            if(x==grid.size()-1 && y==grid[0].size()-1)
-                return t[2];
+// 			// Destination found
+//             if(x==grid.size()-1 && y==grid[0].size()-1)
+//                 return t[2];
 
-            if(grid[x][y]==1){
-                if(t[3]>0) // If we encounter an obstacle and we can remove it
-                    t[3]--;
-                else
-                    continue;
-            }
+//             if(grid[x][y]==1){
+//                 if(t[3]>0) // If we encounter an obstacle and we can remove it
+//                     t[3]--;
+//                 else
+//                     continue;
+//             }
 			
-			// The cell was previously visited by some path and we were able to remove more obstacles from the previous path.
-			// Then we don't need to continue on our current path
-            if(vis[x][y]!=-1 && vis[x][y]>=t[3])
-                continue;
-            vis[x][y]=t[3];
+// 			// The cell was previously visited by some path and we were able to remove more obstacles from the previous path.
+// 			// Then we don't need to continue on our current path
+//             if(vis[x][y]!=-1 && vis[x][y]>=t[3])
+//                 continue;
+//             vis[x][y]=t[3];
             
-            q.push({x+1,y,t[2]+1,t[3]});
-            q.push({x,y+1,t[2]+1,t[3]});
-            q.push({x-1,y,t[2]+1,t[3]});
-            q.push({x,y-1,t[2]+1,t[3]});
+//             q.push({x+1,y,t[2]+1,t[3]});
+//             q.push({x,y+1,t[2]+1,t[3]});
+//             q.push({x-1,y,t[2]+1,t[3]});
+//             q.push({x,y-1,t[2]+1,t[3]});
             
-        }
-        return -1;
-    }
+//         }
+//         return -1;
+//     }
+    
+//     int shortestPath(vector<vector<int>>& grid, int k) {
+//         return solve(grid,k);
+    
+        
+        
+        ////DFS with DP///////
+        
+        int dp[42][42][1609];
+    const static int MAX = 1e7;
     
     int shortestPath(vector<vector<int>>& grid, int k) {
-        return solve(grid,k);
+        return help(grid, grid.size() - 1, grid[0].size() - 1, k, k);
+    }
     
+    int help(vector<vector<int>>& grid, int x, int y, int z, int k){
+        
+        //Reached destination
+        if(x == 0 && y  == 0)return 0;
+        
+        //Already visited/calculated
+        if(dp[x][y][z]) return dp[x][y][z];
+        
+        //Cant remove any more obstacles
+        if(grid[x][y] == 1 && z == 0)return MAX;
+        
+        //Manhattan optimization
+        if(z >= x + y)return dp[x][y][z] = x+y;
+        
+        //Make sure to take direction left and up before right and top
+        vector<vector<int>> dirs = {{-1,0}, {0,-1}, {1,0}, {0,1}};
+        
+        //Setting dp[x][y][z] to MAX so that it does not gets calculated again 
+        dp[x][y][z] = MAX;
+        
+        for(auto dir:dirs){
+            //DFS valid condition
+            if(x + dir[0] >= 0 && x + dir[0] < grid.size() && y + dir[1] >=0 && y + dir[1] < grid[0].size() ){
+                dp[x][y][z] = min(dp[x][y][z], help(grid, x + dir[0], y + dir[1], z - grid[x][y], k) + 1);
+            }
+        }
+        //If not possible
+        if(z == k && x == grid.size() - 1 && y == grid[0].size() - 1 && dp[x][y][z] == MAX)return -1;
+        return dp[x][y][z];
+        
+        
+        
+        
     }
 };
