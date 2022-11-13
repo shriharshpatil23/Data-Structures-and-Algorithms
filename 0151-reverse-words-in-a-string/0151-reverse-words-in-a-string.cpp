8b4840b67195b0d3dcc445e67325f74513d15816
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2022-11-13 12:27:25 +0530

    Time: 48 ms (5.02%), Space: 49.3 MB (5.33%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        vector<string> vec;
+        string tmp;
+        while(getline(ss,tmp,' ')){
+            vec.push_back(tmp);
+        }
+        string ans = "";
+        int n = vec.size();
+        for(int i=n-1;i>-1;i--){
+            if(vec[i].size()!=0){
+                ans = ans  + vec[i] + " ";
+            }
+        }
+        ans.pop_back();
+        return ans;
+    }
+};
